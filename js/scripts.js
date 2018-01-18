52938990f80b5e803b542328958a488cae98a046
Aleksander Chromik <achromik@gmail.com>
2018-01-18 22:06:34 +0100

    Add navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,102 @@
-// $(function () {
-//     console.log($('.welcome_name'));
-
-// });
+var target = location.hash,
+    target = target.replace('#', ''),
+    offset = $('nav').outerHeight();
 
 (function () {
+    var win = $('html, body');
 
-    //handle form submit
+    if (target) {
+        $(win).animate({
+            // scrollTop: $("#" + target).offset().top 
+            scrollTop: $("#" + target).offset().top - offset
+        }, 700, 'swing', function () {
+            waypoints();
+        });
+    } else {
+        waypoints();
+    }
+
+    //Smooth scroll on menu click
+    $('.nav-link[href^="#"]').each(function () {
+        var target = $($(this).attr('href'));
+
+        $(this).click(function () {
+            win.stop().animate({ scrollTop: target.offset().top - offset }, 600);
+            return false;
+        });
+    });
+})();
+
+
+function waypoints() {
+
+    //Setup
+    var lastId,
+        // topMenu = $("#navigation"),
+        menuItems = $("#navigation").find("a.nav-link"),
+        scrollItems = menuItems.map(function (index, val) {
+            var itemHref = $(val).attr("href");
+            if (itemHref.substring(0, 1) === "#") {
+                var item = $(itemHref);
+                if (item.length) {
+                    return item;
+                }
+            }
+        });
+
+    //remove space from name in section 'header'
+    var $pageName = $('.page-name');
+    $pageName.map(function (item, value) {
+        value.firstChild.textContent = value.firstChild.textContent.trim();
+    });
+
+    //Do magic on scroll
+    $(window).on('scroll', function () {
+
+        changeMenuVisibility();
+
+        // Get container scroll position
+        var fromTop = $(this).scrollTop() + offset;
+
+        // Get id of current scroll item
+        var cur = scrollItems.map(function () {
+            if ($(this).offset().top <= fromTop + 1)
+                return this;
+        });
+
+        // Get the id of the current element
+        cur = cur[cur.length - 1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (lastId !== id) {
+            lastId = id;
+            // Set/remove active class
+            $('#navigation li').removeClass('active');
+            $('#navigation li a[href="#' + id + '"]').parent().addClass('active');
+
+            //change url
+            if (history.pushState) {
+                history.pushState(null, null, '#' + id);
+            }
+        }
+    });
+}
+
+function changeMenuVisibility() {
+    var $navbarBrand = $('.navbar-brand');
+    if ($(window).scrollTop() > $(window).height() - 2 * offset) {
+        $('.bg').addClass('visible');
+        $navbarBrand.addClass('visible');
+    } else {
+        $navbarBrand.removeClass('visible');
+        $('.bg').removeClass('visible');
+    }
+}
+
+function handleFormSubmit() {
+
     emailjs.init("user_6VSJRTVkfpALXvVSH1C32");
+
     $('#contact_form').on('submit', function (event) {
         event.preventDefault();
         var $special = $('.special')[0].value.length;
@@ -30,4 +120,23 @@
                 });
         }
     });
+
+}
+
+
+
+(function () {
+    changeMenuVisibility();
+
+
+
+    // $(window).scroll(function () {
+    //     changeMenuVisibility();
+    // });
+
+
+
+
+
+
 })();
